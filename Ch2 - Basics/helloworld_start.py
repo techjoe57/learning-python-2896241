79656dde43055ca10c7cab58c9da153e84f7efe4
techjoe57 <neequayjesse@gmail.com>
2023-03-22 12:18:50 +0000

    Change output format

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -6,7 +6,8 @@
 def main():
     print("Hello World")
     name = input("What is your name?")
-    print("Nice to meet you",name)
+    print(f"Nice to meet you",name) # Output format by tutorial
+    print(f"Nice to meet you {name}") # My output format
     
 if __name__ == "__main__":
     main()
